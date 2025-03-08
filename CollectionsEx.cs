487e4f1385e92eb169794476aebdcb99a6fae621
udayasri-18 <153915797+udayasri-18@users.noreply.github.com>
2025-03-08 14:21:00 +0530

    Add files via upload

---
+++ CollectionsEx.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace M1Prep
+{
+    class CollectionsEx
+    {
+        public void arrayEx()
+        {
+            ArrayList arrayList = new ArrayList();
+            arrayList.Add("we are");
+            arrayList.Add("learning");
+            arrayList.Add(true);
+
+            bool isTrue = (bool)arrayList[2];
+            //List<int> list = new List<int>();
+            for (int i = 0; i < arrayList.Count; i++)
+            {
+                Console.WriteLine(arrayList[i]);
+            }
+        }
+        public void Display()
+        {
+            int[] numbers = new int[5];
+            int length = numbers.Length;
+            int min = 0;
+            for (int i = 0; i < length; i++)
+            {
+                Console.WriteLine($"Enter the value for numbers[{i}]");
+                numbers[i] = int.Parse(Console.ReadLine());
+                if (i == 0)
+                {
+                    min = numbers[0];
+                }
+            }
+        }
+        //public static void Main(string[] args)
+        //{
+        //    CollectionsEx collections = new CollectionsEx();
+        //    //collections.Display();
+        //    collections.arrayEx();
+        //}
+    }
+}
---
+++ ExceptionHandling.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M1Prep
+{
+    class ExceptionHandling
+    {
+        static void Main(string[] args)
+        {
+
+            int[] arr = new int[5];
+            try
+            {
+                //arr[6] = 10;
+
+                //int a = 10; int b = 0;
+                //int c = a / b;
+                //Console.WriteLine(c);
+
+                int x=int.MaxValue;
+                checked
+                {
+                    int y = x + 1;
+                }
+            }
+
+            catch (IndexOutOfRangeException ex)
+            {
+                Console.WriteLine("Index out of range");
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("Divide by zero exception");
+            }
+            catch (ArithmeticException ex)
+            {
+                //Console.WriteLine("Stack overflow ");
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error occured while processing the given input");
+            }
+            finally
+            {
+                Console.WriteLine("Finally block executed");
+            }
+        }
+    }
+}
